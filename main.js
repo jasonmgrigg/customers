69233a626c099f9982514267bbfb35dfd1124c95
Jason Grigg <boatdreamin@gmail.com>
2017-06-14 14:19:01 -0400

    css styling

--- main.js
+++ main.js
@@ -15,43 +15,62 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
 
 
     response.json().then(function(data){
-      console.log(data.results);
-    for (i = 0; i < data.results.length; i++) {
+    // for (i = 0; i < data.results.length; i++) {
+    //
+    // let name = ((data.results[i].name.first) + " " + (data.results[i].name.last));
+    // let emailAddress = data.results[i].email;
+    // let street = data.results[i].location.street;
+    // let cityInfo = ((data.results[i].location.city) + " " + (data.results[i].location.state) + " " + (data.results[0].location.postcode))
+    // let picture = data.results[i].picture.medium;
+    //
+    //     return name;
+    //     return emailAddress;
+    //     return street;
+    //     return cityInfo;
+    //     return picture;
+    //
+    //
 
-    let name = ((data.results[i].name.first) + " " + (data.results[i].name.last));
-    let emailAddress = data.results[i].email;
-    let street = data.results[i].location.street;
-    let cityInfo = ((data.results[i].location.city) + " " + (data.results[i].location.state) + " " + (data.results[0].location.postcode))
-    let picture = data.results[i].picture.medium;
-
-        console.log(name);
-        console.log(emailAddress);
-        console.log(street);
-        console.log(cityInfo);
-        console.log(picture);
 
 
+    let persons = data.results;
+    // console.log(persons);
 
-function renderName(){
-  return `
-  <ul>
-    ${name.map(name => `<li>${name}`).join('')}
-    </ul>
-    `;
-}
+      function renderPeople(){
+        return `
+            ${persons.map(person => `
+              <ul>
+              <li>${person.picture.medium}</li>
+              <li>${person.name.first + " " + person.name.last}</li>
+              <li>${person.email}</li>
+              <li>${person.location.street}</li>
+              <li>${person.location.city + " " + person.location.state + " " + person.location.postcode}</li>
+              <li>${person.phone}</li>
+              </ul>
+              
+              `).join('')}
+                `;
 
-let markup = `
-  <div class="customers">
-    <p>${name}</p>
+        }
 
-    ${renderName(data.results)}
 
-    </div>
-    `
+          let markup = `
+            <div class="customers">
+                ${renderPeople(data.results)}
+            </div>
+              `
 
-document.body.innerHTML = markup;
+        document.body.innerHTML = markup;
 
-}
+
+
+
+
+
+
+
+
+// }
 
   })
 });
